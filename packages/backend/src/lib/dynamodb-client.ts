a22485e1fcb6f1123bcd4ad014bcf25bd1384941
iampeterbanjo <me@iampeterbanjo.com>
2023-06-28 11:56:58 +0100

    test(logs): silence logs during tests

--- packages/backend/src/lib/dynamodb-client.ts
+++ packages/backend/src/lib/dynamodb-client.ts
@@ -1,8 +1,5 @@
 import {DynamoDBClient, DynamoDBClientConfig} from '@aws-sdk/client-dynamodb';
-import {
-  DynamoDBDocumentClient,
-  ExecuteStatementCommand,
-} from '@aws-sdk/lib-dynamodb';
+import {DynamoDBDocumentClient} from '@aws-sdk/lib-dynamodb';
 const config: DynamoDBClientConfig = {
   region: process.env['AWS_REGION'] || 'eu-west-2',
   credentials: {
--- packages/backend/src/photos/api.ts
+++ packages/backend/src/photos/api.ts
@@ -1,7 +1,7 @@
 import fastify from 'fastify';
 import {getPhotos} from '../lib';
 
-const server = fastify({logger: true});
+const server = fastify({logger: process.env.NODE_ENV !== 'test'});
 server.get('/photos', async () => {
   const result = await getPhotos();
   return result.Items;
