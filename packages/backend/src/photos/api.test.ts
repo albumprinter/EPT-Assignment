0f769cb459509683f5aaa2799dc6859933fb12d2
iampeterbanjo <me@iampeterbanjo.com>
2023-06-28 16:37:11 +0100

    refactor(test): make test description concise

--- packages/backend/src/photos/api.test.ts
+++ packages/backend/src/photos/api.test.ts
@@ -36,49 +36,48 @@ const mockDbOutputPhotos = {
 };
 const mockDynamodbSend = db.partiQL.send as jest.Mock;
 
-describe('api', () => {
-  beforeEach(jest.resetAllMocks);
+beforeEach(jest.resetAllMocks);
 
-  it('should respond', async () => {
-    const response = await api.inject({
-      method: 'GET',
-      path: '/photos',
-    });
-
-    expect(response.statusCode).toBeTruthy();
+test('given GET /photos then api responds', async () => {
+  const response = await api.inject({
+    method: 'GET',
+    path: '/photos',
   });
 
-  it(`should respond with 200 statusCode
-  And 3 photos when GET /photos
+  expect(response.statusCode).toBeTruthy();
+});
+
+test(`given GET /photos
+  then api should respond with 200 statusCode
+  And 3 photos
   And response data is correct`, async () => {
-    mockDynamodbSend.mockResolvedValue(mockDbOutputPhotos);
+  mockDynamodbSend.mockResolvedValue(mockDbOutputPhotos);
 
-    const {statusCode, body} = await api.inject({
-      method: 'GET',
-      path: '/photos',
-    });
-
-    const data = JSON.parse(body);
-
-    expect(statusCode).toEqual(200);
-    expect(data).toHaveProperty('length', 3);
-    expect(mockDynamodbSend).toHaveBeenCalledWith(
-      expect.any(ExecuteStatementCommand)
-    );
-    expect(data[0]).toEqual(
-      expect.objectContaining({
-        id: 'image_10',
-        orderCount: expect.any(Number),
-        category: expect.any(String),
-      })
-    );
-    expect(data[0]).toHaveProperty(
-      'extra',
-      expect.objectContaining({
-        texture: expect.any(String),
-        border: expect.any(Number),
-        rotate: expect.any(Number),
-      })
-    );
+  const {statusCode, body} = await api.inject({
+    method: 'GET',
+    path: '/photos',
   });
+
+  const data = JSON.parse(body);
+
+  expect(statusCode).toEqual(200);
+  expect(data).toHaveProperty('length', 3);
+  expect(mockDynamodbSend).toHaveBeenCalledWith(
+    expect.any(ExecuteStatementCommand)
+  );
+  expect(data[0]).toEqual(
+    expect.objectContaining({
+      id: 'image_10',
+      orderCount: expect.any(Number),
+      category: expect.any(String),
+    })
+  );
+  expect(data[0]).toHaveProperty(
+    'extra',
+    expect.objectContaining({
+      texture: expect.any(String),
+      border: expect.any(Number),
+      rotate: expect.any(Number),
+    })
+  );
 });
